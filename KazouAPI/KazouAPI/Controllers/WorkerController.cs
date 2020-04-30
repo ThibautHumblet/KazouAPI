70fd546bbcc68781ea7442c3be95fbdc956ea51b
Thibaut Humblet <thibaut.humblet@telenet.be>
2020-04-30 19:52:16 +0200

    Added CRUD operations to workers

--- KazouAPI/KazouAPI/Controllers/WorkerController.cs
+++ KazouAPI/KazouAPI/Controllers/WorkerController.cs
@@ -21,5 +21,49 @@ namespace KazouAPI.Controllers
         {
             return context.Workers.ToList();
         }
+
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult GetWorker(int id)
+        {
+            Worker worker = context.Workers.Find(id);
+            if (worker == null)
+                return NotFound();
+            return Ok(worker);
+        }
+
+        [HttpPost]
+        public IActionResult CreateWorker([FromBody] Worker newWorker)
+        {
+            context.Workers.Add(newWorker);
+            context.SaveChanges();
+            return Created("", newWorker);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateWorker([FromBody] Worker updateWorker)
+        {
+            Worker originalWorker = context.Workers.Find(updateWorker.WorkerID);
+            if (originalWorker == null)
+                return NotFound();
+
+            originalWorker.FirstName = updateWorker.FirstName;
+            originalWorker.LastName = updateWorker.LastName;
+            originalWorker.BirthDay = updateWorker.BirthDay;
+            context.SaveChanges();
+            return Ok(originalWorker);
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public IActionResult DeleteWorker(int id)
+        {
+            Worker worker = context.Workers.Find(id);
+            if (worker == null)
+                return NotFound();
+            context.Workers.Remove(worker);
+            context.SaveChanges();
+            return NoContent();
+        }
     }
 }
