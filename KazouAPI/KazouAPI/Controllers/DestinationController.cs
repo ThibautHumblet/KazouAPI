b066e00e1df3a087a90648580e5ec94216e1bb3a
Thibaut Humblet <thibaut.humblet@telenet.be>
2020-05-01 11:32:20 +0200

    Added CRUD destinationController

--- KazouAPI/KazouAPI/Controllers/DestinationController.cs
+++ KazouAPI/KazouAPI/Controllers/DestinationController.cs
@@ -21,5 +21,49 @@ namespace KazouAPI.Controllers
         {
             return context.Destinations.ToList();
         }
+
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult GetDestination(int id)
+        {
+            Destination destination = context.Destinations.Find(id);
+            if (destination == null)
+                return NotFound();
+            return Ok(destination);
+        }
+
+        [HttpPost]
+        public IActionResult CreateDestination([FromBody] Destination newDestination)
+        {
+            context.Destinations.Add(newDestination);
+            context.SaveChanges();
+            return Created("", newDestination);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateDestination([FromBody] Destination updateDestination)
+        {
+            Destination originalDestination = context.Destinations.Find(updateDestination.DestinationID);
+            if (originalDestination == null)
+                return NotFound();
+
+            originalDestination.Name = updateDestination.Name;
+            originalDestination.Country = updateDestination.Country;
+            originalDestination.Region = updateDestination.Region;
+            context.SaveChanges();
+            return Ok(originalDestination);
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public IActionResult DeleteDestination(int id)
+        {
+            Destination destination = context.Destinations.Find(id);
+            if (destination == null)
+                return NotFound();
+            context.Destinations.Remove(destination);
+            context.SaveChanges();
+            return NoContent();
+        }
     }
 }
