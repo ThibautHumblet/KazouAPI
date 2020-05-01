d1ab4e8d2e5237bcb2cd9a8b3d048b5cd8a4607b
Thibaut Humblet <thibaut.humblet@telenet.be>
2020-05-01 11:17:31 +0200

    Added CRUD to VacationController

--- KazouAPI/KazouAPI/Controllers/VacationController.cs
+++ KazouAPI/KazouAPI/Controllers/VacationController.cs
@@ -22,5 +22,50 @@ namespace KazouAPI.Controllers
         {
             return context.Vacations.Include(i => i.Destination).ToList();
         }
+
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult GetVacation(int id)
+        {
+            Vacation vacation = context.Vacations.Find(id);
+            if (vacation == null)
+                return NotFound();
+            return Ok(vacation);
+        }
+
+        [HttpPost]
+        public IActionResult CreateVacation([FromBody] Vacation newVacation)
+        {
+            context.Vacations.Add(newVacation);
+            context.SaveChanges();
+            return Created("", newVacation);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateVacation([FromBody] Vacation updateVacation)
+        {
+            Vacation originalVacation = context.Vacations.Find(updateVacation.VacationID);
+            if (originalVacation == null)
+                return NotFound();
+
+            originalVacation.Name = updateVacation.Name;
+            originalVacation.DestinationID = updateVacation.DestinationID;
+            originalVacation.StartDate = updateVacation.StartDate;
+            originalVacation.EndDate = updateVacation.EndDate;
+            context.SaveChanges();
+            return Ok(originalVacation);
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public IActionResult DeleteVacation(int id)
+        {
+            Vacation vacation = context.Vacations.Find(id);
+            if (vacation == null)
+                return NotFound();
+            context.Vacations.Remove(vacation);
+            context.SaveChanges();
+            return NoContent();
+        }
     }
 }
